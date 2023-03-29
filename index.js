4604c60d6d67a01f425ce2dc0597dbcd170c8339
Crisandro Basoy <35168198+Crisandro@users.noreply.github.com>
2023-03-30 07:35:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const corsOption = {
-    origin: "http://localhost:3000",
+    origin: "https://crisandro.github.io/SimpleChat-frontend/",
     methods: ["GET", "POST"],
     credentials: true,
     optionsSuccessStatus: 200
